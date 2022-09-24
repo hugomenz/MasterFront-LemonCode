16aed1f420953fc38b23ff68417930e3ad3725f3
Hugo Martin <hugomartin.menz@gmail.com>
2022-09-24 05:44:18 +0200

    optional waiting for review

---
+++ module3-Webpack/opcional/src/holamundoComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./mystyles.scss";
+
+export const HolaComponent = () => {
+  return (
+    <div className={classes["whitesmoke-bgn"]}>
+      <h2>Hola Mundo!</h2>
+    </div>
+  );
+};
