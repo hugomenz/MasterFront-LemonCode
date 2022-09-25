2b222c21dc074435810fa6d62c80d25891bf8ef2
Hugo Martin <hugomartin.menz@gmail.com>
2022-09-25 07:20:35 +0200

    extra column & href to github profile

--- module4-React/app github users/src/app.tsx
+++ module4-React/app github users/src/app.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { MembersTable } from "./members-table";
+
+interface MemberEntity {
+  avatar_url: string;
+  id: string;
+  login: string;
+}
 
 export const App = () => {
-  return <h1>Hello World!</h1>;
+  const [members, setMembers] = React.useState<MemberEntity[]>([]);
+
+  return (
+    <>
+      <MembersTable org="lemoncode" />;
+    </>
+  );
 };
---
+++ module4-React/app github users/src/member-row.model.ts
@@ -0,0 +1,6 @@
+export interface Member {
+  avatar_url: string;
+  id: string;
+  login: string;
+  html_url: string;
+}
