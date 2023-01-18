3352ee456bf9c0368601de978fda5cf10b547e7d
Hugo Martin <hugomartin.menz@gmail.com>
2023-01-18 16:46:12 +0100

    Added Material UI

--- module4-React/LAB_basico/src/pods/login/login.component.tsx
+++ module4-React/LAB_basico/src/pods/login/login.component.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import { AuthLayout } from "@/layouts/auth.layout";
 import css from "./login.styles.css";
 
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 interface Props {
   onLogin: (username: string, password: string) => void;
 }
@@ -15,6 +27,7 @@ export const Login: React.FC<Props> = (props) => {
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(infoForm);
     setInfoForm({
       ...infoForm,
       [e.target.name]: e.target.value,
@@ -27,6 +40,58 @@ export const Login: React.FC<Props> = (props) => {
 
   return (
     <AuthLayout>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="user"
+              autoFocus
+              value={infoForm.user}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={infoForm.password}
+              onChange={handleChange}
+            />
+
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </AuthLayout>
+  );
+};
+
+{
+  /* 
       <h3>Login</h3>
       <form onSubmit={handleSubmit}>
         <div className={css.loginContainer}>
@@ -45,6 +110,5 @@ export const Login: React.FC<Props> = (props) => {
           <button type="submit">Login</button>
         </div>
       </form>
-    </AuthLayout>
-  );
-};
+       */
+}
