850ab680fbb525f90545028772f0f37a02a63a55
Hugo Martin <hugomartin.menz@gmail.com>
2023-01-18 14:10:02 +0100

    Handled filters for empty strings

--- module4-React/LAB_basico/src/pods/detail/detail.mappers.ts
+++ module4-React/LAB_basico/src/pods/detail/detail.mappers.ts
@@ -1,14 +1,14 @@
 import * as am from "./api/api.models";
 import * as vm from "./detail.vm";
 
-export const mapMemberDetailToVm = (
-  member: am.MemberDetailEntity
-): vm.MemberDetailEntity => {
-  return {
-    id: member.id,
-    login: member.login,
-    name: member.name.replace(/([A-Z])/g, " $1").trim(),
-    company: member.company.replace("@", ""),
-    bio: member.bio,
-  };
+export const mapMemberDetailToVm = (member: am.MemberDetailEntity): vm.MemberDetailEntity => {
+  if (member) {
+    return {
+      id: member.id,
+      login: member.login,
+      name: member.name.replace(/([A-Z])/g, " $1").trim(),
+      company: member.company.replace("@", ""),
+      bio: member.bio,
+    };
+  }
 };
--- module4-React/LAB_basico/src/pods/list/api/list.api.ts
+++ module4-React/LAB_basico/src/pods/list/api/list.api.ts
@@ -1,7 +1,7 @@
 import { MemberEntity } from "../list.vm";
 
 export const getMemberCollection = (org): Promise<MemberEntity[]> => {
-  return fetch(`https://api.github.com/orgs/${org}/members`).then((response) =>
-    response.json()
-  );
+  if (org !== "") {
+    return fetch(`https://api.github.com/orgs/${org}/members`).then((response) => response.json());
+  }
 };
